2b231eb0f4bae208b97f2df87fa527ae74cfe07b
Pankaja23 <63946019+Pankaja23@users.noreply.github.com>
2021-07-12 11:26:23 +0530

    Created convert.java
    This file convert minutes into numbers of years and days

---
+++ convert.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int years,days;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of minutes =");
+        int minutes = sc.nextInt();
+
+        years =  (minutes / (60 * 24 * 365));
+        days  =  (minutes / 60 / 24) % 365;
+
+        System.out.println(minutes + " is approximately " + years + " years and " + days + " days");
+
+    }
+}
