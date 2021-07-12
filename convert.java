ae04f0c5b61265f9aa2c6813cd993ed211b7c78d
Pankaja23 <63946019+Pankaja23@users.noreply.github.com>
2021-07-12 15:17:06 +0530

    Update convert.java

--- convert.java
+++ convert.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Enter number of minutes =");
         int minutes = sc.nextInt();
 
+        
         years =  (minutes / (60 * 24 * 365));
         days  =  (minutes / 60 / 24) % 365;
 
